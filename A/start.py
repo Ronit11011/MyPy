5ca491dcc0185199a293926f499fc3a9e225c1eb
Ronit Bhatt <85544778+Ronit11011@users.noreply.github.com>
2021-09-01 23:23:09 +0530

    Create start.py

---
+++ A/start.py
@@ -0,0 +1,13 @@
+a=20
+b=3.14
+c="Ronit"
+d=None
+# printing the defined variables
+print(a,b,c,d,sep="\n")
+# set is used to set the separator type it can be " ","\n" or any other thing.
+# printing the type of variables(which type it is)i.e int or float or bool or string etc.
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
