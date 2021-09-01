1b8e1ddc72c1bb2a0bcad1e04ebf61535a0a92ee
Ronit Bhatt <85544778+Ronit11011@users.noreply.github.com>
2021-09-01 23:33:34 +0530

    Update operators.py

--- A/operators.py
+++ A/operators.py
@@ -1,3 +1,4 @@
+# Here only two operators are mentioned since rest of 'em are quite simple i.e arithmetic ,assignment operators etc.
 # comparison operators
 print(13>=13)
 print(14!=15)
@@ -5,6 +6,6 @@ print(14!=15)
 # logic operators
 a=True
 b=False
-print("ans",a and b)
-print("ans",a or b)
+print("ans",a and b)#true &false-->false
+print("ans",a or b)#true || false -->true
 print("ans", not b)
