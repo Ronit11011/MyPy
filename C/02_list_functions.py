cab3c988fe4443dddb4e6f88735e9040105027bf
Ronit Bhatt <85544778+Ronit11011@users.noreply.github.com>
2021-09-02 20:16:34 +0530

    Add files via upload

---
+++ C/02_list_functions.py
@@ -0,0 +1,12 @@
+a=[30,20,10,50,40,60,70]
+a.sort() #sorts the list
+
+a.reverse()#reverses the list
+
+a.append(90)#adds in the last of the list
+
+a.insert(3,700)#adds elements in the given index--->"list.insert(index,elemnt)"
+
+a.remove(50)#remove the element
+
+a.pop(2)#removes element at index 2
