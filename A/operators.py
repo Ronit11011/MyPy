bbad34d9da2062b514c35809ad627ef71476c98b
Ronit Bhatt <ronitbhatt360@gmail.com>
2021-09-03 11:38:10 +0530

    Update operators.py

--- A/operators.py
+++ A/operators.py
@@ -1,4 +1,3 @@
-# Here only two operators are mentioned since rest of 'em are quite simple i.e arithmetic ,assignment operators etc.
 # comparison operators
 print(13>=13)
 print(14!=15)
@@ -6,6 +5,15 @@ print(14!=15)
 # logic operators
 a=True
 b=False
-print("ans",a and b)#true &false-->false
-print("ans",a or b)#true || false -->true
+print("ans",a and b)
+print("ans",a or b)
 print("ans", not b)
+
+# floor division "//"
+c=5//2 #return the floor value i.e 2
+print(c)
+
+# exponential " ** " (return y power of x in x**y)
+d=2**5
+print(d)
+
