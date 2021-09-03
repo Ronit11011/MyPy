0cdea906ffe9d9652697df635445062ec077f2d8
Ronit Bhatt <ronitbhatt360@gmail.com>
2021-09-03 11:38:47 +0530

    Update typecast.py

--- A/typecast.py
+++ A/typecast.py
@@ -5,3 +5,12 @@ str(a)
 int(b)
 print(type(a))
 print(type(b))
+# IMPLICIT TYPE CONVERSION :- it automatically converts one data type to another ithout any user involve ment needed
+a=int(90.5) #data loss will take place(lower to higher dt conversion)
+print(a)
+# python avoids data loss in implicit type conversion
+
+# EXPLICIT TYPE CONVERSION
+# user convert the dat type of an object to required dat type
+b=int('90')
+print(b)
