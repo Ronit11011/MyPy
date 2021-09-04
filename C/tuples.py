6a6c2adb01016dfff26c8c3c31db3b0d819a5c0d
Ronit Bhatt <ronitbhatt360@gmail.com>
2021-09-04 11:53:17 +0530

    Create tuples.py

---
+++ C/tuples.py
@@ -0,0 +1,11 @@
+# Tuple is an immutable(cannot change) data type in python.
+a=(1,2,3,4,5) #tuple
+print(a)
+# unlike in list its elements cannot be change
+a[2]=45 # !error!
+
+# declaring tuple with single element
+t1=(1)#wrong way to declare
+t1=(1,) #right way
+print(t1)
+                 
