c369a222ad541e5532e47e2570cb7674155bca39
Ronit Bhatt <ronitbhatt360@gmail.com>
2021-09-07 22:27:58 +0530

    Create 02_in_and_is.py

---
+++ E/02_in_and_is.py
@@ -0,0 +1,16 @@
+# is
+a=None
+if(a is None):  # is is used to check the crosscheck the value of a single  variable
+    print("a is none")
+else:
+        print("Not None")
+        
+#  in
+l=[4,5,6,7]
+print(10 in l) #will return true if 10 is present in l else will return false
+
+x=int(input())
+if x in l:
+    print("present")
+else:
+    print("not present")    
