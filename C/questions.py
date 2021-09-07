423d73a8cf4d82961670d94d9d98ff804f0b29e5
Ronit Bhatt <ronitbhatt360@gmail.com>
2021-09-07 11:01:52 +0530

    Create questions.py
    Novice questions on lists

---
+++ C/questions.py
@@ -0,0 +1,19 @@
+# create a list of fuits enterered by user
+f1=input("Enter the fruit 1: ")
+f2=input("Enter the fruit 2: ")
+f3=input("Enter the fruit 3: ")
+f4=input("Enter the fruit 4: ")
+f5=input("Enter the fruit 5: ")
+f6=input("Enter the fruit 6: ")
+mylist=[f1,f2,f3,f4,f5,f6]
+print(mylist)
+
+# create a sorted list of the marks of the students entered
+m1=int(input("marks 1:"))
+m2=int(input("marks 2:")) 
+m3=int(input("marks 3:"))
+m4=int(input("marks 4:"))
+m5=int(input("marks 5:"))
+marks=[m1,m2,m3,m4,m5]
+marks.sort()
+print(marks)
