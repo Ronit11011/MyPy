96f247565fabc58760005507db127c54741f59c4
Ronit Bhatt <ronitbhatt360@gmail.com>
2021-09-07 22:26:58 +0530

    Create 01_Conditional.py
    Conditional statements

---
+++ E/01_Conditional.py
@@ -0,0 +1,20 @@
+# if-elif-else
+# simple odd even example
+a=int(input("enter a no."))
+if(a%2)==0:
+    print("even")
+else:
+    print("odd")
+
+    # syntax for if-elif-else
+a=30
+if(a>20):
+    print("first")
+elif(a>40):
+    print("second")
+else:
+    print("Last")    
+
+    # one liner if-else
+    a=20
+    print("even" if a%2==0 else "odd")
