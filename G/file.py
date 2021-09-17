bb878e7dd2c2ce024aedb55b02b91936e6a6ba39
Ronit Bhatt <ronitbhatt360@gmail.com>
2021-09-17 21:28:37 +0530

    Create file.py

---
+++ G/file.py
@@ -0,0 +1,13 @@
+ '''
+ type of files:-
+1.Text files:-(.txt)
+2.Binary files:-(.jpg ,.dat,etc.)
+
+READING A FILE IN PYTHON-
+f=open("this.txt","r")-->open the file
+text=f.read()-->Read its contents
+print(text)-->Print its contents
+f.close()-->close the file   
+
+f.readline()-->Read one line from the file
+'''
